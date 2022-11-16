b44f04d1869fc5b3596ed3554e6485e27487da6c
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-11-16 16:15:29 +0200

    simple shell

--- main.c
+++ main.c
@@ -1,34 +1,46 @@
 #include "holberton.h"
-
 /**
- * main - principal function
- * @argc: is an int
- * @argv: is a char
- * @environ: global variable
- * Return: 0
- */
+* main - Short description, single line
+* @ac: number of args
+* @av: args
+* @env: current enviroment
+* Return: Description of the returned value
+*/
 
-int main(int argc, char **argv, char **environ)
+int main(int ac, char *av[], char *env[])
 {
-	char *line = NULL;
-	char *delim = "\t \a\n";
-	char *command;
-	char **tokens;
-	(void)argc;
+int bytesRead, x = 0, t = 0, z = -1;
+size_t buffsize;
+char *buffer = NULL, **tokens = NULL, **commands = NULL;
+h_t *head = NULL;
 
-	tokens = find_path(environ);
-
-	signal(SIGINT, SIG_IGN);
+if (ac != 1)
+{ filecommands(av, env, t);
+return (0);
+} signal(SIGINT, siggy);
 	while (1)
-	{
-		line = read_line();
-		argv = splits(line, delim);
-		command = args_path(argv, tokens);
-		if (command == NULL)
-			execute(argv);
-		free(line);
-		free(argv);
-		free(command);
+	{ t = 0;
+	if (isatty(STDIN_FILENO))
+	_puts("#cisfun$ ");
+	bytesRead = getline(&buffer, &buffsize, stdin);
+	if (bytesRead == -1)
+	break;
+	x = berautix() - 1;
+	make_history(buffer, &head, x);
+	buffer[bytesRead - 1] = '\0';
+	if (*buffer == '\0')
+	continue;
+	z = isbuiltin(buffer, env, &head);
+	if (z == 1 || z == 4 || z == 3)
+	continue;
+	if (z == 2)
+	free(buffer), free_nodes(head), _exit(0);
+	if (spacecheck(buffer) == 0)
+	continue;
+	tokenizing(tokens, commands, av, env, buffer, t);
 	}
-	return (0);
+	writehistory(head);
+	free_nodes(head);
+	free(buffer);
+return (0);
 }
