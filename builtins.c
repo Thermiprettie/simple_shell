c6079ffe2ecf80b379a1dae41f7cb3cbfa9bcc56
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-11-16 18:54:20 +0200

    simple shell

--- builtins.c
+++ builtins.c
@@ -1,130 +1,30 @@
-#include "shell.h"
-
-#define SETOWD(V) (V = _strdup(_getenv("OLDPWD")))
+#include "holberton.h"
 /**
- * change_dir - changes directory
- * @data: a pointer to the data structure
- *
- * Return: (Success) 0 is returned
- * ------- (Fail) negative number will returned
- */
-int change_dir(sh_t *data)
-{
-	char *home;
+**shell_exit - exits the shell
+**Return: void
+**/
 
-	home = _getenv("HOME");
-	if (data->args[1] == NULL)
+	int shell_exit(void)
 	{
-		SETOWD(data->oldpwd);
-		if (chdir(home) < 0)
-			return (FAIL);
-		return (SUCCESS);
+		return (-1);
 	}
-	if (_strcmp(data->args[1], "-") == 0)
-	{
-		if (data->oldpwd == 0)
-		{
-			SETOWD(data->oldpwd);
-			if (chdir(home) < 0)
-				return (FAIL);
-		}
-		else
-		{
-			SETOWD(data->oldpwd);
-			if (chdir(data->oldpwd) < 0)
-				return (FAIL);
-		}
-	}
-	else
-	{
-		SETOWD(data->oldpwd);
-		if (chdir(data->args[1]) < 0)
-			return (FAIL);
-	}
-	return (SUCCESS);
-}
-#undef GETCWD
+
+
 /**
- * abort_prg - exit the program
- * @data: a pointer to the data structure
- *
- * Return: (Success) 0 is returned
- * ------- (Fail) negative number will returned
- */
-int abort_prg(sh_t *data __attribute__((unused)))
-{
-	int code, i = 0;
+**shell_env - prints environment
+**Return: void
+**/
 
-	if (data->args[1] == NULL)
+	int shell_env(void)
 	{
-		free_data(data);
-		exit(errno);
-	}
-	while (data->args[1][i])
-	{
-		if (_isalpha(data->args[1][i++]) < 0)
+		unsigned int i;
+
+		i = 0;
+		while (environ[i] != NULL)
 		{
-			data->error_msg = _strdup("Illegal number\n");
-			return (FAIL);
+			write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
+			i++;
 		}
+		return (0);
 	}
-	code = _atoi(data->args[1]);
-	free_data(data);
-	exit(code);
-}
-/**
- * display_help - display the help menu
- * @data: a pointer to the data structure
- *
- * Return: (Success) 0 is returned
- * ------- (Fail) negative number will returned
- */
-int display_help(sh_t *data)
-{
-	int fd, fw, rd = 1;
-	char c;
-
-	fd = open(data->args[1], O_RDONLY);
-	if (fd < 0)
-	{
-		data->error_msg = _strdup("no help topics match\n");
-		return (FAIL);
-	}
-	while (rd > 0)
-	{
-		rd = read(fd, &c, 1);
-		fw = write(STDOUT_FILENO, &c, rd);
-		if (fw < 0)
-		{
-			data->error_msg = _strdup("cannot write: permission denied\n");
-			return (FAIL);
-		}
-	}
-	PRINT("\n");
-	return (SUCCESS);
-}
-/**
- * handle_builtin - handle and manage the builtins cmd
- * @data: a pointer to the data structure
- *
- * Return: (Success) 0 is returned
- * ------- (Fail) negative number will returned
- */
-int handle_builtin(sh_t *data)
-{
-	blt_t blt[] = {
-		{"exit", abort_prg},
-		{"cd", change_dir},
-		{"help", display_help},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while ((blt + i)->cmd)
-	{
-		if (_strcmp(data->args[0], (blt + i)->cmd) == 0)
-			return ((blt + i)->f(data));
-		i++;
-	}
-	return (FAIL);
-}
